01282572256159bcad665228f46f76a7b7675b1d
Maggie Leavell <mjleavell@gmail.com>
2018-09-22 12:09:30 -0500

    complete encode input

--- main.js
+++ main.js
@@ -2,5 +2,3 @@ const printToDom = (stringToPrint, elementId) => {
     const selectedDiv = document.getElementById(elementId);
     selectedDiv.innerHTML = stringToPrint;
 };
-
-printToDom('hi', 'decode');
